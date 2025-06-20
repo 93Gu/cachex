46f4edaafbeb43376f5debbeef8ca834f746dba0
93Gu <100657006+93Gu@users.noreply.github.com>
2025-06-20 14:55:29 +0800

    Update main.go

--- example/main.go
+++ example/main.go
@@ -1,1 +1,25 @@
+package main
+
+import (
+	"cachex/cache"
+	"context"
+	"fmt"
+	"time"
+)
+
+func main() {
+	local, _ := cache.NewLocal(1<<20, time.Minute)
+	redis := cache.NewRedis("localhost:6379", "", 0)
+	c := cache.NewHybridCache(local, redis)
+
+	val, err := c.Get(context.Background(), "user:123", func() (any, error) {
+		fmt.Println("fetch from DB")
+		return "jack", nil
+	}, time.Minute)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("got:", val)
+}
 
